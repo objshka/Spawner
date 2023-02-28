93dc67f03a3715f83ccb7528984618744fd6cdaa
objshka <yazkiri4@gmail.com>
2023-03-01 00:15:09 +0300

    firstTry

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Coin _prefab;
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private float _sleepTime;
+
+    private WaitForSeconds _delayTime;
+    private int _randomPosition;
+
+    public void Start()
+    {
+        _delayTime = new WaitForSeconds(_sleepTime);
+        StartCoroutine(SpawnCoins());
+    }
+
+    private IEnumerator SpawnCoins()
+    {
+        while (true)
+        {
+            _randomPosition = Random.Range(0, _spawnPoints.Length);
+            Instantiate(_prefab, _spawnPoints[_randomPosition].transform.position, Quaternion.identity);
+            
+            yield return _delayTime;
+        }
+    }
+}
